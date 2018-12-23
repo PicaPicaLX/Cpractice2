eccb4dd02dd2e0696ce0b3cd5b3edd3d10449de5
栗鑫 <lx389029980@outlook.com>
2018-12-23 13:25:21 +0800

    添加项目文件。

---
+++ Cpractice2/CP2_1.c
@@ -0,0 +1,44 @@
+﻿#include <stdio.h>
+int main()
+{
+	/*UseExpr();*/
+	UseOperator();
+}
+
+int UseExpr()
+{
+	int iNumber1, iNumber2, iNumber3;
+	iNumber1 = 3; /*为iNumber1赋值为3*/
+	iNumber2 = 7; /*为iNumber2赋值为7*/
+	printf("The first number is: %d\n", iNumber1);
+	printf("The second number is: %d\n", iNumber2);
+
+	iNumber3 = iNumber1 + 10; /*iNumber1加10赋值给iNumber3*/
+	printf("The first number add 10 is: %d\n", iNumber3);
+
+	iNumber3 = iNumber2 + 10; /*iNumber2加10赋值给iNumber3*/
+	printf("The second number add 10 is: %d\n", iNumber3);
+
+	iNumber3 = iNumber1 + iNumber2; /*iNumber1加iNumber2赋值给iNumber3*/
+	printf("The resule of first add second is: %d\n", iNumber3);
+
+	system("pause");
+	return 0;
+}
+
+int UseOperator()
+{
+	int iHoursWorded = 8;
+	int iHourlyRate;
+	int iGrossPay;
+
+	iHourlyRate = 13;
+	iGrossPay = iHoursWorded * iHourlyRate;
+
+	printf("The HoursWorded is: %d\n", iHoursWorded);
+	printf("The HourlyRate is: %d\n", iHourlyRate);
+	printf("The GrossPay is: %d\n", iGrossPay);
+
+	system("pause");
+	return 0;
+}
